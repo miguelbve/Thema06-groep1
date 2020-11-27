8971dec0ed79afdf95ead3d6f0446b8c7cb8b0f9
miguelbve <miguelbotter@hotmail.com>
2020-11-27 12:28:55 +0100

    Bugs fixed tools_installer and added remove option

--- tools_installer.py
+++ tools_installer.py
@@ -1,7 +1,7 @@
 """
 Pre-installed command-line tools on bioinf.nl:
 Picard:             picard-tools
-Hisat:              hisat2
+HISAT:              hisat2
 FastQC:             fastqc
 SAMtools:           samtools
 
@@ -17,6 +17,7 @@ __date__ = "2020-11-26"
 
 
 import subprocess
+import argparse
 
 def install_tools():
     # change to the home directory, here the source files of the command-line tools will be installed
@@ -27,23 +28,50 @@ def install_tools():
     f"pip3 install multiqc && "
     # install Trim Galore
     f"curl -LO https://github.com/FelixKrueger/TrimGalore/archive/0.6.6.zip && unzip 0.6.6.zip && rm -rf 0.6.6.zip && "
-    # install the Subread package that includes featureCounts, exactSNP, subindel, subjunc, sublong, subread-align and subread-buildindex
+    # install Subread package that includes featureCounts, exactSNP, subindel, subjunc, sublong, subread-align and subread-buildindex
     f"curl -LO https://sourceforge.net/projects/subread/files/subread-2.0.1/subread-2.0.1-Linux-x86_64.tar.gz &&" 
     f"tar -xzvf subread-2.0.1-Linux-x86_64.tar.gz && rm -rf subread-2.0.1-Linux-x86_64.tar.gz && "  
                    
-    # adds source paths to .bash_profile, this allows the user of this program to run all the installed command line tools
+    # create a .bash_profile, and add the paths that redirect to the installed tools
     f"echo 'source ~/.bashrc\n"
     f"export PATH=$PATH:~/.local/bin\n"
     f"export PATH=$PATH:~/TrimGalore-0.6.6\n"
-    f"export PATH=$PATH:~/subread-2.0.1-Linux-x86_64/bin' >> ~/.bash_profile && source ~/.bash_profile && "
+    f"export PATH=$PATH:~/subread-2.0.1-Linux-x86_64/bin' > ~/.bash_profile && source ~/.bash_profile && "
     
     # change to the initial directory
     f"cd -", shell=True, executable="/bin/bash") # executes the commands through using the bash shell
 
 
+def uninstall_tools():
+    # change to the home directory, here the source files of the command-line tools are installed
+    subprocess.run(f"cd ~ && "
+    # uninstall Cutadapt
+    f"pip3 uninstall cutadapt && "
+    # uninstall MultiQC
+    f"pip3 uninstall multiqc && "
+    # uninstall Trim Galore
+    f"rm -rf TrimGalore-0.6.6 && "
+    # uninstall Subread package that includes featureCounts, exactSNP, subindel, subjunc, sublong, subread-align and subread-buildindex
+    f"rm -rf subread-2.0.1-Linux-x86_64 && "
+    # remove the .bash_profile, since all the tools are uninstalled. We don't need the specified paths anymore.
+    f"rm -rf .bash_profile && source ~/.bashrc && "
+    
+    # change to the initial directory
+    f"cd - && exec bash", shell=True, executable="/bin/bash")
+
+
+
 def main():
-    # Install all the required pipe line command line tools
-    install_tools()
+    parser = argparse.ArgumentParser(description="install and configure missing cli tools for the M. Dubbelaar pipeline")
+    parser.add_argument('-u', '--uninstall', help="uninstall and deconfigure cli tools for the M. Dubbelaar pipeline",
+                        action='store_true')
+    args = parser.parse_args()
+    if args.uninstall:
+        uninstall_tools()
+        print("Successfully uninstalled TrimGalore-0.6.6\n"
+              "Successfully uninstalled subread-2.0.1-Linux-x86_64")
+    else:
+        install_tools()
 
 
 if __name__ == "__main__":
