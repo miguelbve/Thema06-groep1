316390b72d1f1547bb0a65c2876b8d3cf03ac518
mrsikkema <74648787+mrsikkema@users.noreply.github.com>
2020-12-09 12:01:33 +0100

    Add files via upload
    trying to use the data from a text file

--- code/fastqc.py
+++ code/fastqc.py
@@ -1,35 +1,43 @@
-"""
-Reads log files to determine data file paths and use them as arguments for the fastqc CLI tool
+read = open('fastqc_data.txt')
+switch = False
+summary = []
+thing = []
+things = []
+for line in read:
+    if line.startswith('>>'):
+        if switch == True:
+            switch = False
+            things.append(thing)
+            thing = []
+            summary.append(line.strip())
+        else:
+            switch = True
+    if switch:
+        thing.append(line.strip())
 
-usage:
-    python3 fastqc.py       program file
-"""
+dicti = {}
+for a, i in enumerate(things):
+    if a == 3: #het nummer van het grafiekje waar je data van wil
+        for b, i in enumerate(i):
+            if b > 2: #meer dan 1, laat de header en kopjes weg
+                ye = i.split('\t')[0:5]
+                new = []
+                for c, i in enumerate(ye):
+                    if c in dicti.keys():
+                        dicti[c].append(float(i))
+                    else:
+                        dicti[c] = [float(i)]
 
-import subprocess
-import argparse
-import multiqc
-
-
-def execute_fastqc():
-    """ Execute fastqc and write the output to the data directories """
-    with open('../logs/data_path.log', 'r') as pathlog, open('../logs/fastq_files.log', 'r') as fastqlog:
-        data_path = pathlog.readline()
-        fastqdirs = [("../results/fastqc/" + file[0:-4]) for file in fastqlog]
-        fastqlog.seek(0) # set the current read position back to the first line
-        fastqfiles = [(data_path+file[0:-1]) for file in fastqlog]
-        for fastqfile, fastqdir in zip(fastqfiles, fastqdirs):
-            subprocess.run(["fastqc", fastqfile, '-o', fastqdir], text=True)
-
-def summarize_read_qual():
-    """ Summarize the read qualities with a multiqc """
-    multiqc.run("../results/fastqc/", outdir="../results/fastqc/", make_pdf=True, no_data_dir=True)
-
-
-def main():
-    """ main """
-    # execute_fastqc()
-    summarize_read_qual()
-
-
-if __name__ == "__main__":
-    main()
+lijst = []
+for i in range(1, 5):
+    for a, num in enumerate(dicti[i]):
+        if not a == 0:
+            diff = num - old   #prints the difference in a c t g percentages per nucleotide
+            if diff > 1.5:
+                print('yes')
+            else:
+                print('no')
+        old = num
+    print('')
+        
+        
