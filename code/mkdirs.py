5129feae7018baca9553d545cb789ed0dfe32ef7
miguelbve <miguelbotter@hotmail.com>
2020-12-09 15:57:13 +0100

    run mkdirs.py needs now mkdirs.py /data/storix2/student/2020-2021/Thema06/project-data/ as second argument since here an apart folder for groep1 will be added that holds huge data processing data files

--- code/mkdirs.py
+++ code/mkdirs.py
@@ -9,7 +9,7 @@ SRR018013_1.fastq.gz  SRR018015.fastq.gz  SRR057599.fastq.gz   SRR1106122.fastq.
 SRR018013_2.fastq.gz  SRR057598.fastq.gz  SRR1106118.fastq.gz  SRR1106138.fastq.gz  SRR1106140.fastq.gz
 
 usage:
-    python3 mkdirs /data/storix2/student/2019-2020/Thema06/project-data/How_to_deal_with_difficult_data/Data/
+    python3 mkdirs.py /data/storix2/student/2019-2020/Thema06/project-data/How_to_deal_with_difficult_data/Data/ /data/storix2/student/2020-2021/Thema06/project-data/
 
 output:
     scanned_fastq_files.log     file
@@ -30,6 +30,10 @@ def log_data_path(data_path):
     with open('../logs/data_path.log', 'w') as file_obj:
         file_obj.write(data_path)
 
+def log_storage_path(storage_path):
+    with open('../logs/storage_path.log', 'w') as file_obj:
+        file_obj.write(storage_path)
+
 def make_fastq_dirs():
     """ Makes data directories and names them based on their .fastq.gz file extension """
     with open('../logs/fastq_files.log') as file_obj:
@@ -44,20 +48,38 @@ def make_trim_galore_dirs():
         fastqdirs = ["../results/trim_galore/"+file[0:-4] for file in file_obj]
         subprocess.run(["rm", "-vrf", "results/trim_galore"], text=True) # remove if exist, notify user
         for dir in fastqdirs:
-            subprocess.run(["mkdir", "-vp", dir], text=True) # make fastqc dirs, notify user
+            subprocess.run(["mkdir", "-vp", dir], text=True)
+
+
+## These dirs can be found in the external_storage path. For more info ##
+def make_ref_genomes_dirs():
+    with open('../logs/storage_path.log') as file_obj:
+        dir = file_obj.readline()
+        subprocess.run(["mkdir", "-vp", dir+"groep1/ref_genomes"], text=True)
+
+def make_ref_genomes_index_dirs():
+    with open('../logs/storage_path.log') as file_obj:
+        dir = file_obj.readline()
+        subprocess.run(["mkdir", "-vp", dir+"groep1/ref_genomes_index"], text=True)
+
 
 def main():
     """ main """
     parser = argparse.ArgumentParser()
     parser.add_argument("data_path")
+    parser.add_argument("storage_path")
     args = parser.parse_args()
 
     # remove log file if exist, otherwise create them and notify user
     subprocess.run("rm -vrf ../logs; mkdir -v ../logs", shell=True, executable="/bin/bash")
     log_data_files(args.data_path)
     log_data_path(args.data_path)
+    log_storage_path(args.storage_path)
+
     make_fastq_dirs()
     make_trim_galore_dirs()
+    make_ref_genomes_dirs()
+    make_ref_genomes_index_dirs()
 
 
 if __name__ == "__main__":
